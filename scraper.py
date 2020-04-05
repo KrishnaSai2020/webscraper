cf402fea8cd5799d45c01fd93dad03c0b41cef69
kc <krishnasai.chivukula@gmail.com>
2020-04-05 14:51:26 +0100

    does all 2 pages of it now

--- scraper.py
+++ scraper.py
@@ -1,10 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
-res = requests.get('https://news.ycombinator.com/newest')
+res = requests.get('https://news.ycombinator.com')
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.storylink')
 subtext = soup.select('.subtext')
+
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
 
 
 def create_custom_hacker_news(links, subtext):
@@ -15,9 +20,17 @@ def create_custom_hacker_news(links, subtext):
         vote = subtext[idx].select('.score')
         if len(vote):
             points = int(vote[0].getText().replace(" points", "").replace('point', ''))
-            hn.append({'title': title, 'link': href, 'votes': points})
+            if points > 30:
+                hn.append({'title': title, 'link': href, 'votes': points})
 
-    return hn
+    return sort_stories_by_votes(hn)
 
 
-create_custom_hacker_news(links, subtext)
+pprint.pprint(create_custom_hacker_news(links, subtext))
+
+res = requests.get('https://news.ycombinator.com/news?p=2')
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.select('.storylink')
+subtext = soup.select('.subtext')
+
+pprint.pprint(create_custom_hacker_news(links, subtext))
