30dd89563cad6e3a1032d861d06e0b9e71779dc2
kc <krishnasai.chivukula@gmail.com>
2020-04-05 14:23:41 +0100

    n/a

--- scraper.py
+++ scraper.py
@@ -2,4 +2,21 @@ import requests
 from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com/newest')
-print(res)
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.select('.storylink')
+votes = soup.select('.score')
+
+
+def create_custom_hacker_news(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        title = links[idx].getText()
+        href = links[idx].get('href',None)
+        points = int(votes[idx].getText().replace(' points', ''))
+
+        hn.append({'title': title, 'link': href})
+
+    return hn
+
+
+print(create_custom_hacker_news(links, votes))
